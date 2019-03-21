6e93597e6a5f6a7ed7e0b8b63552525926a10b1f
jtt049 <jtt049@ucsd.edu>
2019-03-21 13:19:30 -0700

    Add files via upload

--- Bluetooth2/Bluetooth2.ino
+++ Bluetooth2/Bluetooth2.ino
@@ -8,43 +8,39 @@
 
 const int MPU_addr=0x68;
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
-
 int minVal=265;
 int maxVal=402;
-
 double x;
 double y;
 double z;
 
-
 // Signals set
-int LED_Left = 3;
+int LED_Left = 7;
 int LED_Right = 5;
 int Buttom_Left = 4;
 int Buttom_Right = 6;
-int R_value = 0;
-int L_value = 0;
+int R_value;
+int L_value;
+int previousR=LOW;
+int previousL=LOW; 
 int stateRight; 
 int stateLeft;
 
-
-
 // Break set
 int LED = 8;
-int Matrix = 9;
-int Trig = 10;
-int Echo = 11;
+int Trig = 9;
+int Echo = 10;
 long Time;
-int DisCm;
-int DisInch;
-int BreakDis;
-
+double DisCm;
+double DisInch;
+double BreakDis;
+int stopplease; 
 
 // Library to make a Software UART
 #include <SoftwareSerial.h>
 
-#define TX 2
-#define RX 3
+#define TX 12
+#define RX 13
  
 SoftwareSerial BTSerial(RX, TX); // (RX, TX)
 
@@ -63,8 +59,7 @@ void setup() {
 
   // HM-10 virtual UART
   BTSerial.begin(BAUDRATE);  
-
-
+  
   // gyro
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -80,29 +75,26 @@ void setup() {
   pinMode(Buttom_Right, INPUT);
 
   // Break
-  Serial.begin(9600);
   pinMode(LED, OUTPUT);
-  pinMode(Matrix, OUTPUT);
   pinMode(Trig, OUTPUT);
   pinMode(Echo, INPUT);
   
 }
  
-void loop() {  
-  // Transmit data via bluetooth
+void loop() {   
   bluetooth_transmit();  
-
+  delay(500);  
   // Necessary forced delay, if we transmit too fast (no delay)
   //  the error rate increases
-  delay(1);
+ 
 }
 
 // Function responsible for transmitting data over bluetooth
 void bluetooth_transmit() {
   // Update data to be transmitted
-  pkt.a = 1;
-  pkt.b = 0;
-  pkt.c = 0;
+  pkt.a = left();
+  pkt.b = stopNow();
+  pkt.c = right();
     
   // Write packet data to the bluetooth - and transmit
   BTSerial.write((byte *) & pkt,sizeof(Packet));
@@ -112,24 +104,132 @@ void bluetooth_transmit() {
 }
 
 int left(){
+  L_value = digitalRead(Buttom_Left);
+  if(L_value == LOW && previousL == HIGH){
+    if(stateLeft == 0){
+      digitalWrite(LED_Left,HIGH);
+      stateLeft = 1;
+      digitalWrite(LED_Right,LOW);
+      stateRight = 0; 
+      delay(10);
+    }
+    else{
+      digitalWrite(LED_Left,LOW);
+      stateLeft = 0; 
+    }
+  }
+  previousL = L_value;
+  gyrocheck();
   return stateLeft; 
 }
 
+
 int right(){
+  R_value = digitalRead(Buttom_Right);
+  if(R_value == LOW && previousR== HIGH){
+    if(stateRight == 0){
+      digitalWrite(LED_Right,HIGH);
+      stateRight = 1; 
+      digitalWrite(LED_Left,LOW);
+      stateLeft = 0; 
+      delay(10); 
+    }
+    else{
+    digitalWrite(LED_Right,LOW);
+    stateRight = 0; 
+    }
+  }
+  previousR = R_value; 
+  gyrocheck();
   return stateRight; 
 }
 
+int stopNow() {
+  breakfunction();
+  if (BreakDis <= 2.2){
+    digitalWrite(LED, HIGH);
+    stopplease = 1; 
+   }
+  else{
+    digitalWrite(LED, LOW);
+    stopplease = 0; 
+  }
+  //breakprint();
+  return stopplease; 
+}
+
+void breakfunction(){
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trig, LOW);
+ 
+  Time = pulseIn(Echo, HIGH);
+  //DisCm= (Time/2) / 29.1;
+  //DisInch = (Time/2) / 73.914;
+  DisInch= Time*0.034/2;
+  // Prints the distance on the Serial Monitor
+  //Serial.print("Distance: ");
+  //Serial.println(DisInch);
+  BreakDis = DisInch;
+}
 
 
+void breakprint(){
+  //Serial.print("Distance Cm: ");
+  //Serial.print(DisCm);
+  Serial.print("   DistanceInch: ");
+  Serial.println(DisInch);  
+}
+
+void gyrocheck(){
+  gyro();
+  if(x>=10 && x<=340){
+    digitalWrite(LED_Right,LOW);
+    stateRight = 0; 
+    digitalWrite(LED_Left,LOW);
+    stateLeft = 0; 
+  }
+}
 
 
+void gyro(){
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);
+  AcX=Wire.read()<<8|Wire.read();
+  AcY=Wire.read()<<8|Wire.read();
+  AcZ=Wire.read()<<8|Wire.read();
+  int xAng = map(AcX,minVal,maxVal,-90,90);
+  int yAng = map(AcY,minVal,maxVal,-90,90);
+  int zAng = map(AcZ,minVal,maxVal,-90,90);
+  x= RAD_TO_DEG * (atan2(-yAng, -zAng)+PI);
+  y= RAD_TO_DEG * (atan2(-xAng, -zAng)+PI);
+  z= RAD_TO_DEG * (atan2(-yAng, -xAng)+PI);
+  print_gyro();
+ 
+}
+
+void print_gyro() {
+  Serial.print("AngleX= ");
+   Serial.print(x);
+  
+   Serial.print("  AngleY= ");
+   Serial.print(y);
+  
+   Serial.print("  AngleZ= ");
+   Serial.println(z);
+   Serial.println("-----------------------------------------");
+}
 
 
 // Function to print packet data (debug)
 void print_packet() {  
-  Serial.print("TX: (a,b,c,d)=(");
+  Serial.print("TX: (a,b,c)=(");
   Serial.print(pkt.a); Serial.print(",");
   Serial.print(pkt.b); Serial.print(",");
-  Serial.print(pkt.c); Serial.print(",");  
+  Serial.print(pkt.c);
   Serial.println(")");
 }
